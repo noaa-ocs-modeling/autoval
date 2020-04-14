72b1fe4b47b5a40da3b79dfd7d695a2794f0ca34
Sergey Vinogradov <grapesh@gmail.com>
2020-04-14 11:30:08 -0400

    Initial commit

---
+++ autoval/validate/exp.load.py
@@ -0,0 +1,19 @@
+"""
+@author: Sergey.Vinogradov@noaa.gov
+"""
+# Loads model output of a single experiment
+# according to the INI file
+
+# Read INI file
+
+# Locate the COMOUT
+
+# Read the requested field
+
+# Return the field
+
+def field ( fileName, ncField):
+    return field
+    
+    
+    
