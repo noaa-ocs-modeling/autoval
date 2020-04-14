95d51844ef3092b128fa69c5c061c0492103bf9a
Sergey Vinogradov <grapesh@gmail.com>
2020-04-14 11:27:30 -0400

    Initial commit

---
+++ autoval/__init__.py
@@ -0,0 +1,6 @@
+from . import report
+from . import validate
+from . import plot
+
+__all__ = ['report','validate','plot','tests']
+
---
+++ autoval/report/toc.update.py
@@ -0,0 +1,15 @@
+"""
+@author: Sergey.Vinogradov@noaa.gov
+"""
+
+# Read INI file.
+
+# Locate TOC file.
+
+# Find a handle for the run in the TOC file.
+
+# Add/update row(s) for the experiment.
+
+# Save an updated TOC file locally.
+
+# Upload/update onto remote server if needed.
