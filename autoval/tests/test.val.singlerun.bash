9c04b146d85029b599df9ed6db7957aba47f55ae
Sergey Vinogradov <svinogra@voyager.local>
2020-04-14 12:59:28 -0400

    Work on config files

---
+++ autoval/tests/test.val.singlerun.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Set mount point
+mount="/Users/svinogra"
+
+# Set path to python3 
+pyPath=${mount}"/anaconda3/bin/python"
+
+# Set path to python executable
+myCode=${mount}"/GitHub/autoval/autoval/validate/run.py"
+
+# Set PYTHONPATH to csdllib
+PYTHONPATH=${mount}"/GitHub/csdllib"
+
+# Specify the INI file
+iniFile=${mount}"/GitHub/autoval/tests/singlerun.al182012_hsofs.ini"
+
+# Execute
+PYTHONPATH=${PYTHONPATH} ${pyPath} -W ignore ${myCode} -i ${iniFile}
