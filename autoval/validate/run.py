0333a4d1f367c41e9f79ba3964733fe2f80cb566
Sergey Vinogradov <svinogra@voyager.local>
2020-04-15 10:35:14 -0400

    wip

--- autoval/validate/run.py
+++ autoval/validate/run.py
@@ -27,7 +27,13 @@ def check_comout (comout):
     else:
         return 1   
 #==============================================================================
-def singlerun (cfg):
+def singleRun (cfg):
+    """
+    Performs validation of a single given run,
+    Returns diagnostic fields (diagFields) for multirun analysis, if requested
+    """
+    grid       = []
+    diagFields = []
 
     # 1. Check model Path
     comout = cfg['Experiment']['path']
@@ -35,18 +41,27 @@ def singlerun (cfg):
         pass # exit here
     
     # 2. Parse cfg on what to do
+    
 
     # 3. Call out validation, save
     # 4. Call out report generator
     # 5. Call out plotting
+    return grid, diagFields 
 
 #==============================================================================
 if __name__ == "__main__":
 
     cmd = read_cmd_argv          (sys.argv[1:])
     cfg = csdllib.oper.sys.config (cmd.iniFile)
+    print (cfg) 
+
     if cfg['Mode']['multirun']:
+        print (cfg['Mode']['multirun'])
         # Assemble multirun setup here
-        pass
+        # Call singlerun for each experiment, collect diagFields
+        for exp in experiments:
+            grid, diagFields = singleRun (cfg)
+        # Perform multirun diagnostics here
+
     else:
-        singlerun (cfg)
+        singleRun (cfg)
