8170f0eb5ad66e657141497790272500346dccf2
Sergey Vinogradov <svinogra@voyager.local>
2020-04-18 17:56:54 -0400

    wip

--- autoval/validate/waterlevel.py
+++ autoval/validate/waterlevel.py
@@ -1,8 +1,10 @@
 """
 @author: Sergey.Vinogradov@noaa.gov
 """
-import glob
+import os, glob
 import csdllib
+from csdllib.oper.sys import stampToTime, timeToStamp, msg
+from datetime import datetime
 
 #==============================================================================
 def timeSeriesStats (pointFile, cfg):
@@ -24,28 +26,66 @@ def waterlevel (cfg, path):
     Returns diagnostic fields (diagFields) for multirun analysis, if requested
     """
     pointStats = []
+
     # Point data (time series, hardwired to COOPS tide gauges)
     if cfg['Analysis']['pointdatastats']:
         fmask = cfg['WaterLevel']['pointfilemask']
         masks = fmask.split(',')
-        pointFile = []
+        pointsFile = []
         for m in masks:
             f = glob.glob(path + m)
             if len(f):
-                pointFile.append(f[0])
-        if len(pointFile)>1:
+                pointsFile.append(f[0])
+        if len(pointsFile)>1:
             msg['w','Found more than 1 point output. Verify your mask!']
-            for f in pointFile:
+            for f in pointsFile:
                 print(f)
-        pointFile = pointFile[0]
+        pointsFile = pointsFile[0]
         
-        pointStats = timeSeriesStats (pointFile, cfg)
+        # Read list of stations
+        model = \
+            csdllib.models.adcirc.readTimeSeries (pointsFile, ncVar = 'zeta', verbose=1)
+        stations = model['stations']
+        if len(stations) == 0:
+            msg('w','No stations found')
         
-    # 2. Parse cfg on what to do
-    
+        # Set datespan
+        dates = model['time']
+        datespan = [dates[0], dates[-1]] 
+        try:
+            datespan[0] = stampToTime (cfg['WaterLevel'].get('pointdatesstart'))
+        except:
+            pass
+        try:
+            datespan[1] = stampToTime (cfg['WaterLevel'].get('pointdatesend'))
+        except:
+            pass
+        msg ( 'i','Datespan for analysis is set to: ' \
+            + timeToStamp(datespan[0]) + ' ' + timeToStamp(datespan[1]) )
+        
+        # Download / read COOPS stations data
+        for n in range(len(stations)):
+            forecast  = model['zeta'][:,n]
+            nosid     = stations[n].strip()
+            localFile = os.path.join(
+                        cfg['Analysis']['localdatadir'], 
+                        'cwl.nos.' + nosid + '.' + \
+                        timeToStamp(datespan[0]) + '-' + \
+                        timeToStamp(datespan[1]) + '.dat')    
+            
+            if not os.path.exists(localFile):
+                obs = csdllib.data.coops.getData(nosid, datespan)
+                csdllib.data.coops.writeData (obs, localFile)
+            else:
+                obs = csdllib.data.coops.readData(localFile)
 
-    # 3. Call out validation, save
-    # 4. Call out report generator
-    # 5. Call out plotting
+            # Perform analysis 
+            refDates, obsVals, modVals = \
+                csdllib.methods.interp.retime  (           \
+                    datespan, obs['values'], model['time'],\
+                    forecast, refStepMinutes=6 )
+            metrics = csdllib.methods.statistics.metrics (\
+                obsVals, modVals, refDates)
+            print (metrics)
 
     return pointStats
