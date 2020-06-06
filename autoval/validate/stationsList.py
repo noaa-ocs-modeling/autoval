77424402dad076db2d722256a4904f256cb6e887
Sergey Vinogradov <svinogra@voyager.local>
2020-06-06 11:34:34 -0400

    wip

--- autoval/validate/stationsList.py
+++ autoval/validate/stationsList.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     ncFile = sys.argv[1:][0]
     m = csdllib.models.adcirc.readTimeSeries (ncFile, ncVar = 'zeta', verbose=1)
     for n in range(len(m['lon'])):
-        info = str(m['lon'][n]) + ' ' \
+        info = str(n).zfill(3) + ' ' + str(m['lon'][n]) + ' ' \
              + str(m['lat'][n]) + ' ' \
              + str(m['stations'][n])
         print ( info )
