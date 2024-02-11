3aadf8f5da0cb077ff285a754b560e11f5e89cdd
Atieh Alipour <148251584+AtiehAlipour-NOAA@users.noreply.github.com>
2024-02-11 09:59:20 -0800

    Add files via upload

---
+++ tests/test1_global.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#Script execution instructions
+
+source /work/noaa/nosofs/aalipour/envs/autoval_env/bin/activate  #activate the virtual environment
+
+# Set path to python3 
+pyPath=/work/noaa/nosofs/aalipour/Orion_envs/autoval_env/bin/python
+
+module load nco
+
+
+datetimeLabel="202307171030" #$(date +\%Y\%m\%d\%H\%M)
+currenttime="10:30" #$(date +%H:%M)
+echo $currenttime
+runDate="20230717" 
+
+
+export USER=`whoami`
+
+#Assign the directories
+cwd=/work/noaa/nosofs/aalipour/STOFS_2D_Test/jobs/
+inputDir=/work/noaa/nosofs/aalipour/STOFS_2D_Test/inputs/dynamic/
+configDir=/work/noaa/nosofs/aalipour/STOFS_2D_Test/config/
+procDir=/work/noaa/nosofs/aalipour/STOFS_2D_Test/outputs/www/
+#saveOutDir=/work/noaa/nosofs/aalipour/STOFS_2D_Test/saveOutDir/ 
+host=localhost
+user=$USER
+
+echo $cwd
+echo $inputDir
+echo $procDir
+echo $saveOutDir
+echo $host
+echo $user
+
+#Cleanup previous run
+rm -rf $procDir{./img/*,index.htm,../work/*,../data/*,../tmp/*}
+
+cd $cwd
+
+fileMaxele="stofs_2d_glo.t06z.fields.cwl.maxele.nc"
+filePoints="stofs_2d_glo.t06z.points.cwl.nc"
+
+echo $runDate > maxele.recent.new.txt
+echo $currenttime >> maxele.recent.new.txt
+echo $fileMaxele >> maxele.recent.new.txt
+echo $filePoints >> maxele.recent.new.txt
+
+# Select points for skill assessment
+#ncks -F -d station,1,833 $inputDir/stofs_2d_glo.t18z.points.cwl.nc $inputDir/stofs_2d_glo.t06z.points.autoval.cwl.nc
+ncks -F -d station,1,10 $inputDir/stofs_2d_glo.t18z.points.cwl.nc $inputDir/stofs_2d_glo.t06z.points.autoval.cwl.nc
+
+
+# Set path to python executable
+myCode="/work/noaa/nosofs/aalipour/STOFS_2D_Test/code/autoval/autoval/validate/run.py"
+
+# Set PYTHONPATH to csdllib
+PYTHONPATH="/work/noaa/nosofs/aalipour/STOFS_2D_Test/code/csdllib/"
+
+# Specify the INI file
+iniFile="/work/noaa/nosofs/aalipour/STOFS_2D_Test/config/test1_global.ini"
+
+
+PYTHONPATH=${PYTHONPATH} ${pyPath} -W ignore ${myCode} -p $inputDir/ -i ${iniFile}
+
